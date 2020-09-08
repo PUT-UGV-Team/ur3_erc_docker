8fdc3941b63b076c59c73aab3db76df56373da9b
Mikołaj Łysakowski <mikolaj.lysakowski.bk@gmail.com>
2020-09-08 13:02:24 +0200

    Add realsense and usb-cam support

--- build_erc.sh
+++ build_erc.sh
@@ -1,6 +1,6 @@
 build_erc(){
     echo "Building ERC UR3"
-    docker build -f Dockerfile.ur3_erc --tag ur3_erc --no-cache .
+    docker build -f Dockerfile.ur3_erc --no-cache --tag ur3_erc .
 }
 
 build_erc
